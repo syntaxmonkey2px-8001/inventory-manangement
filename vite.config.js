a5894fa2b4d956aef83a9748901a44bf411ede97
Lia S <eopqrs@gmail.com>
2026-08-27 18:52:56 +0100

    Add Sass support and mock data for inventory management
    - Updated package.json and package-lock.json to include Sass as a dependency.
    - Created mockData.js for inventory items with details
    - Added _variables.scss for styling variables and app.scss for main styles.
    - Modified vite.config.js to set up path alias for easier imports.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve:{
+    alias:{
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
 })
